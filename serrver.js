7c6a4a66192ef3a0f91511c7447386c2c5c524b3
Lukesh Kolhe <lukesh.kolhe123@gmail.com>
2021-05-26 14:37:03 +0530

    heroku

--- serrver.js
+++ serrver.js
@@ -8,7 +8,8 @@ app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/project-name' }
   );
 });
-
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 console.log(`Running on port ${process.env.PORT || 8080}`)
