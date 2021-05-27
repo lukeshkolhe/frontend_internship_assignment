ec134f3d381b6afde4e79f7f96ec847543a04a71
Lukesh Kolhe <lukesh.kolhe123@gmail.com>
2021-05-27 09:33:02 +0530

    procfile

--- src/app/server.js
+++ src/app/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/project-name'));
+app.use(express.static('./dist/frontend'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/project-name' }
+  res.sendFile('index.html', { root: 'dist/frontend' }
   );
 });
 app.listen(process.env.PORT || 3000, function(){
